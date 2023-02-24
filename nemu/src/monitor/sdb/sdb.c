e0780f5c8ac51ffdf913c56a25c6c973bfbd4da2
zqz <zqz@111.com>
2023-02-24 23:02:37 +0800

    optimize PA1 info register

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -60,8 +60,7 @@ static int cmd_si(char *args) {
 
 static int cmd_info(char *args) {
   char *arg = strtok(NULL, " ");
-  if (arg == NULL)  isa_reg_display(arg);
-  else isa_reg_display(arg);
+  isa_reg_display(arg);
   return 0;
 }
 
