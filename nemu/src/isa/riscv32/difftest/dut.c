b1118a23ac9463be9b3896b25e053a9950e0b9a9
zqz <zqz.com>
2023-03-31 20:20:07 +0800

    add difftest function to check the instruction implement

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -18,7 +18,20 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  /* 
+   * difftest: check with right register and pc for every instruction
+   *  for riscv, the absolutely correct implementation tool is Spike
+   */
+  int reg_num = ARRLEN(cpu.gpr);
+  for (int i = 0; i < reg_num; i++) {
+    if (ref_r->gpr[i] != cpu.gpr[i]) {
+      return false;
+    }
+  }
+  if (ref_r->pc != cpu.pc) {
+    return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach() {
