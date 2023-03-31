577131105e5b64fe30becbb8220cc3e0661fcad6
zqz <z@qz.com>
2023-03-31 22:14:22 +0800

    implement keyboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,14 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t k = inl(KBD_ADDR);/* default is AM_KEY_NONE */
+  /*  
+   *  the most significant bit of k is set to 1 
+   *  indicating that the key is being pressed down
+   */
+  kbd->keydown = (k & KEYDOWN_MASK ? true : false);
+  /* 
+   *  the value of each keycode can according to x86/qemu/ioe.c
+   */
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
