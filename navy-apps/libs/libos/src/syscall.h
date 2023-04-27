38ffb647ad00ed8344a3f2bb639eab9d38063467
zqz <z@qz.com>
2023-04-27 11:07:13 +0800

    add syscall in headers

--- navy-apps/libs/libos/src/syscall.h
+++ navy-apps/libs/libos/src/syscall.h
@@ -24,4 +24,32 @@ enum {
   SYS_gettimeofday
 };
 
+char *syscall_names[] = {
+  "exit",
+  "yield",
+  "open",
+  "read",
+  "write",
+  "kill",
+  "getpid",
+  "close",
+  "lseek",
+  "brk",
+  "fstat",
+  "time",
+  "signal",
+  "execve",
+  "fork",
+  "link",
+  "unlink",
+  "wait",
+  "times",
+  "gettimeofday"
+};
+
+int sys_yield();
+int sys_exit();
+int sys_write(int fd, const void *buf, size_t count);
+int sys_brk(void *addr);
+
 #endif
