567dc3c1eb26984a2ab8aff61483a588fe0b9f1b
zqz <z@qz.com>
2023-05-29 09:52:38 +0800

    complete comment

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -8,7 +8,12 @@ static Context* do_event(Event e, Context* c) {
       // Log("EVENT_SYSCALL");
     case EVENT_NULL:
       // Log("EVENT_NULL");
-      c->mepc += 4;
+      /* for self-trap exception(yield), which is used for time-sharing system
+          mepc should be dnpc.
+         for fault-type exception(e.g. page fault, memory fault)
+          The returned PC and the PC that triggered the exception are the same.
+       */
+      c->mepc += 4;//RISC did this by software
       do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
