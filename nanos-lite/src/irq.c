5124ab9f1e6b15876bc773e0ca3130c89c885574
zqz <z@qz.com>
2023-08-07 21:10:34 +0800

    PA4.1:fix the bug in context switch2

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,7 +6,8 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
       // printf("\33[0mirq event yield!\n");
-      return schedule(c);
+      c =  schedule(c);
+      break;
     case EVENT_SYSCALL:
       // Log("EVENT_SYSCALL");
     case EVENT_NULL:
@@ -16,10 +17,11 @@ static Context* do_event(Event e, Context* c) {
          for fault-type exception(e.g. page fault, memory fault)
           The returned PC and the PC that triggered the exception are the same.
        */
-      c->mepc += 4;//RISC did this by software
+      // c->mepc += 4;//RISC did this by software
       do_syscall(c);break;
     default: panic("Unhandled event ID = %d", e.event);
   }
+  c->mepc += 4;//RISC did this by software
 
   return c;
 }
